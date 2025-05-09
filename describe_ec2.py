11e22184838cd6baaa4c9bf89f3b06ba7b85f318
ldogiparthi <157041916+ldogiparthi@users.noreply.github.com>
2025-05-09 14:20:01 -0500

    Update describe_ec2.py

--- describe_ec2.py
+++ describe_ec2.py
@@ -7,7 +7,7 @@ def describe_instance(instance_id):
         'ec2',
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-        aws_session_token=os.getenv('AWS_SESSION_TOKEN')
+        aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
         region_name='us-east-1'
     )
     
